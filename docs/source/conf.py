3ef079058088b777eace15f11de93fbfd91b1ab6
şuayip üzülmez <suayip.541@gmail.com>
2025-09-19 20:12:49 +0300

    Bump version to `0.6.1`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ settings.configure()
 project = "rest-filters"
 copyright = "2025, şuayip üzülmez"
 author = "şuayip üzülmez"
-release = "0.6.0"
+release = "0.6.1"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
